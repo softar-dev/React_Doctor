ef06d6e15ac3b6b9361a62692b16903d1ec03f2f
OzmaKa <oussamahka101@gmail.com>
2026-03-24 12:03:50 +0300

    fix: add ES2020 target to ts-node compiler options to fix __spreadArray error

--- cli/bin/react-doctor.js
+++ cli/bin/react-doctor.js
@@ -18,6 +18,8 @@ require(
   transpileOnly: true,
   skipIgnore: true,
   compilerOptions: {
+    target: "ES2020",
+    lib: ["ES2020"],
     module: "commonjs",
     esModuleInterop: true,
     allowSyntheticDefaultImports: true,
